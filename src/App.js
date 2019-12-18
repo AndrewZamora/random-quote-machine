0ff722ea63b8770f72d22950c523770020a97feb
AndrewZamora <andrewmichaelzamora@gmail.com>
2019-12-18 22:08:38 +0900

    Found quote api and got quotes coming in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Random Quote Machine</h1>
-      <div id="quote-box"></div>
-    </div>
-  );
+const getQuotes = async () => {
+  const response = await fetch('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand');
+  const data = await response.json();
+  return data;
 }
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { quotes: [] };
+  }
+
+  async componentDidMount() {
+    const allQuotes = await getQuotes();
+    this.setState({
+      quotes: allQuotes
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Random Quote Machine</h1>
+        <div id="quote-box">
+          <div id="text">
+          </div>
+          <div id="author"></div>
+          <div id="new-quote">
+            {this.state.quotes.length > 0 ? this.state.quotes[0].id : 'loading'}
+          </div>
+          <a href="" id="tweet-quote"></a>
+        </div>
+      </div>
+    )
+  }
+}
 export default App;
