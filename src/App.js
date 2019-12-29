9c58121ae80ef48d694ebc21184d608a600797a1
AndrewZamora <andrewmichaelzamora@gmail.com>
2019-12-29 22:27:20 +0900

    Added quotes from wikiquotes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react'
 import './App.css';
 
+const getPageIds = async title => {
+  const response = await fetch(`https://en.wikiquote.org/w/api.php?action=query&format=json&origin=*&titles=${title}&generator=links&gplnamespace=0&gpllimit=20`);
+  // couldn't use  response.json() because the response was not in JSON format https://stackoverflow.com/a/40403285/8006073
+  const data = await response.text();
+  return data;
+};
+const getNamesFromPage = async pageId => {
+  const response = await fetch(`https://en.wikiquote.org/w/api.php?action=query&format=json&origin=*&prop=links&pageids=${pageId}&redirects=1&pllimit=max`);
+  const data = await response.text();
+  return data;
+};
+const getQuote = async name => {
+  const response = await fetch(`https://en.wikiquote.org/w/api.php?action=parse&format=json&origin=*&page=${name}&prop=text&section=1&disablelimitreport=1&disabletoc=1`);
+  const data = await response.text();
+  return JSON.parse(data).parse.text['*'];
+};
 const getQuotes = async () => {
   // Quotes come from this API https://quotesondesign.com/api/
   const response = await fetch('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand');
@@ -18,9 +34,17 @@ class App extends Component {
       quotes: [],
       currentQuote: []
     };
+    this.textRef = React.createRef();
+    this.authorRef = React.createRef();
   }
   async componentDidMount() {
     const requestedQuotes = await getQuotes();
+    const test1 = await getPageIds('list of people by name');
+    const test2 = await getNamesFromPage('112861')
+    const test3 = await getQuote('Carson Grant')
+    console.log(test1)
+    console.log(test2)
+    console.log(test3)
     this.setState({
       quotes: requestedQuotes,
       currentQuote: requestedQuotes[0]
@@ -32,13 +56,22 @@ class App extends Component {
       currentQuote: newQuote
     });
   }
-  createQuote = () => {
+  createQuote = (callback) => {
     // This is a REACT way to render HTML String https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
     return (<>
-      <div id="text" dangerouslySetInnerHTML={{ __html: this.state.currentQuote.content.rendered }} />
-      <div id="author" dangerouslySetInnerHTML={{ __html: this.state.currentQuote.title.rendered }} />
-      <button id="new-quote" onClick={() => this.selectQuote()}>New Quote</button>
+      <div id="text" ref={this.textRef} dangerouslySetInnerHTML={{ __html: this.state.currentQuote.content.rendered }} />
+      <div id="author" ref={this.authorRef} dangerouslySetInnerHTML={{ __html: this.state.currentQuote.title.rendered }} />
     </>);
+  }
+  tweetQuote = (quote) => {
+    console.log(quote.current !== null ? quote.current.textContent : "loading")
+    console.log(quote)
+    // return (
+    //   <a href={`https://twitter.com/intent/tweet?text=`} id="tweet-quote" >Tweet</a>
+    // )
+  }
+  getPlainText = (html) => {
+    return html.replace()
   }
   render() {
     return (
@@ -46,7 +79,7 @@ class App extends Component {
         <h1>Random Quote Machine</h1>
         <div id="quote-box">
           {this.state.quotes.length > 0 && this.createQuote()}
-          <a href="" id="tweet-quote"></a>
+          <button id="new-quote" onClick={() => this.selectQuote()}>New Quote</button>
         </div>
       </div>
     )
