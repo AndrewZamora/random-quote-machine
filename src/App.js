02723a76f8a7d0d966b54ae712a2b7a4d477bfe9
AndrewZamora <andrewmichaelzamora@gmail.com>
2020-01-01 01:04:57 +0900

    Removed old quote api functions

--- src/App.js
+++ src/App.js
@@ -3,26 +3,30 @@ import './App.css';
 
 const getPageIds = async title => {
   const response = await fetch(`https://en.wikiquote.org/w/api.php?action=query&format=json&origin=*&titles=${title}&generator=links&gplnamespace=0&gpllimit=20`);
-  // couldn't use  response.json() because the response was not in JSON format https://stackoverflow.com/a/40403285/8006073
-  const data = await response.text();
-  return data;
+  const data = await response.json();
+  return Object.keys(data.query.pages);
 };
 const getNamesFromPage = async pageId => {
   const response = await fetch(`https://en.wikiquote.org/w/api.php?action=query&format=json&origin=*&prop=links&pageids=${pageId}&redirects=1&pllimit=max`);
-  const data = await response.text();
-  return data;
+  const data = await response.json();
+  // Wanted to use data.query.pages[pageId].links but the object's page id returned from API doesn't always match
+  const actualPageId = Object.keys(data.query.pages);
+  const names = data.query.pages[actualPageId].links.map(link => link.title);
+  return names;
 };
 const getQuote = async name => {
   const response = await fetch(`https://en.wikiquote.org/w/api.php?action=parse&format=json&origin=*&page=${name}&prop=text&section=1&disablelimitreport=1&disabletoc=1`);
   const data = await response.text();
   return JSON.parse(data).parse.text['*'];
 };
-const getQuotes = async () => {
-  // Quotes come from this API https://quotesondesign.com/api/
-  const response = await fetch('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand');
-  const data = await response.json();
-  return data;
+const createElement = htmlString => {
+  const element = document.createElement('div');
+  element.innerHTML = htmlString;
+  return element;
 }
+const parseElement = element => {
+  return element.querySelector('ul').querySelector('li').childNodes[0].textContent;
+};
 const randomNum = limit => {
   return Math.floor((Math.random() * limit));
 }
@@ -38,16 +42,16 @@ class App extends Component {
     this.authorRef = React.createRef();
   }
   async componentDidMount() {
-    const requestedQuotes = await getQuotes();
-    const test1 = await getPageIds('list of people by name');
-    const test2 = await getNamesFromPage('112861')
-    const test3 = await getQuote('Carson Grant')
-    console.log(test1)
-    console.log(test2)
-    console.log(test3)
+    const pageIds = await getPageIds('list of people by name');
+    const randomId = pageIds[randomNum(pageIds.length)];
+    const names = await getNamesFromPage(randomId);
+    const randomName = names[randomNum(names.length)];
+    const unparsedQuote = await getQuote(randomName);
+    const quote = parseElement(createElement(unparsedQuote));
     this.setState({
-      quotes: requestedQuotes,
-      currentQuote: requestedQuotes[0]
+      currentAuthor: randomName,
+      currentQuote: quote,
+      names: [{ id: randomId, names: names }]
     })
   }
   selectQuote = () => {
@@ -56,11 +60,12 @@ class App extends Component {
       currentQuote: newQuote
     });
   }
-  createQuote = (callback) => {
+  createQuote = (quote, author) => {
     // This is a REACT way to render HTML String https://reactjs.org/docs/dom-elements.html#dangerouslysetinnerhtml
     return (<>
-      <div id="text" ref={this.textRef} dangerouslySetInnerHTML={{ __html: this.state.currentQuote.content.rendered }} />
-      <div id="author" ref={this.authorRef} dangerouslySetInnerHTML={{ __html: this.state.currentQuote.title.rendered }} />
+      {quote}
+
+      {author}
     </>);
   }
   tweetQuote = (quote) => {
@@ -70,15 +75,12 @@ class App extends Component {
     //   <a href={`https://twitter.com/intent/tweet?text=`} id="tweet-quote" >Tweet</a>
     // )
   }
-  getPlainText = (html) => {
-    return html.replace()
-  }
   render() {
     return (
       <div className="App">
         <h1>Random Quote Machine</h1>
         <div id="quote-box">
-          {this.state.quotes.length > 0 && this.createQuote()}
+          {this.state.currentQuote.length > 0 && this.createQuote(this.state.currentQuote, this.state.currentAuthor)}
           <button id="new-quote" onClick={() => this.selectQuote()}>New Quote</button>
         </div>
       </div>
